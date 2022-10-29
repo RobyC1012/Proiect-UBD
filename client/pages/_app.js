5f28018a895b81be8d087025778b7943ab5afe32
Robert <clintocrobert@yahoo.com>
2022-10-29 11:02:09 +0300

    login update

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,16 +1,10 @@
 import '../public/css/style.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "antd/dist/antd.css";
-import "../public/css/style.css";
-import "react-toastify/dist/ReactToastify.css";
-import { Provider } from "../context";
 
 function MyApp({ Component, pageProps })
 {
     return(
-        <Provider>
-            <Component {...pageProps} />
-        </Provider>
+        <Component {...pageProps} />
     );
 }
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,67 +1,22 @@
-import { useState, useContext, useEffect } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
-import Link from "next/link";
-import { Context } from "../context";
-import { useRouter } from "next/router";
 import { Layout } from "antd";
-import { SyncOutlined } from "@ant-design/icons";
-
-const { Content, Footer, Header, Sider } = Layout;
+import Password from "antd/lib/input/Password";
+const { Content } = Layout;
 
 const Index = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-
-
-    //state
-    const {
-        state:{ user },
-        dispatch
-    } = useContext(Context);
-    // router
-    const router = useRouter();
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            setLoading(true);
-            toast("logging in");
-            const { data } = await axios.post(`/api/login`, {
-            email,
-            password,
-            });
-            // console.log("LOGIN RESPONSE", data);
-            dispatch({
-            type: "LOGIN",
-            payload: data,
-            });
-            // save in local storage
-            window.localStorage.setItem("user", JSON.stringify(data));
-            // redirect
-            router.push("/data");
-            //setLoading(false);
-        } catch (err) {
-            toast(err.response.data);
-            setLoading(false);
-        }
-    };
     return (
         <Content style={{ padding: '0 0px' }}>
             <div className="login-form">
                 <h1> Login </h1>
                 <hr></hr>
                 <div>
-                    <form onSubmit={handleSubmit}>
+                    <form>
                         <div className="input-group">
                             <input
                             type="email"
                             id="email"
                             required className="input"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            
+
                             />
                             <label htmlFor="email" className="input-label">Email address</label>
                         </div>
@@ -71,19 +26,15 @@ const Index = () => {
                             type="password"
                             id="password"
                             required className="input"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            
+
                             />
                             <label htmlFor="password" className="input-label">Password</label>
                         </div>
                         <div className="forgot-password loginf">
                             <a href="#">Forgot password?</a>
                         </div>
-                        <button type="submit" 
-                            className="btn btn-block btn-primary"
-                            disabled={!email || !password || loading}>
-                            {loading ? <SyncOutlined spin /> : "Submit"}
-                        </button>
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                 </div>
             </div>
